5eecad9743dcdaaf758cfc6aa8b412e82f5df1e3
GabrielStancu <gabriel.stancu07@yahoo.com>
2021-09-09 23:12:22 +0300

    Added ActorsController

---
+++ MovieNight/MovieNight/Controllers/ActorsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieNight.Controllers
+{
+    public class ActorsController : Controller
+    {
+        public IActionResult Index()
+        {
+            //TODO: this has to be changed
+            return View();
+        }
+    }
+}
