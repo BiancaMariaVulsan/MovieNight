0689c608a6c129b6b5569ac0b2cf78f77fc1d72e
GabrielStancu <gabriel.stancu07@yahoo.com>
2021-09-09 23:30:38 +0300

    Added movies controller

---
+++ MovieNight/MovieNight/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieNight.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            int a = 7;
+            a++;
+            Console.WriteLine($"a = {a}");
+            return View();
+        }
+
+        public int ReturnSmth()
+        {
+            Console.WriteLine(" I don't know what I am doing...");
+            return -4;
+        }
+    }
+}
